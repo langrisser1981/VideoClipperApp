9b5a29f05df98c6203172c113c965e5a0873dccf
lenny <langrisser1981@gmail.com>
2026-08-20 11:23:43 +0800

    feat: add PlayerContainerView AVPlayerView bridge

---
+++ VideoClipperApp/Sources/Views/PlayerContainerView.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerContainerView.swift
+//  VideoClipperApp
+//
+
+import SwiftUI
+import AVKit
+
+struct PlayerContainerView: NSViewRepresentable {
+    let player: AVPlayer
+
+    func makeNSView(context: Context) -> AVPlayerView {
+        let view = AVPlayerView()
+        view.controlsStyle = .none
+        view.player = player
+        return view
+    }
+
+    func updateNSView(_ nsView: AVPlayerView, context: Context) {
+        if nsView.player !== player {
+            nsView.player = player
+        }
+    }
+}
